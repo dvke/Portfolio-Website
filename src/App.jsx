fcbd855c65e3867e6895afb887285dfa49b39833
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-08-22 13:26:36 +0100

    About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   );
