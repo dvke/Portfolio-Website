6de1e0689df811f708ef36ad7cee0db09e85184a
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-08-24 14:54:31 +0100

    Adding Swiper.js functionality

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -19,6 +19,13 @@ export const Data = [
   },
   {
     id: 3,
+    image: Image2,
+    title: "Harry Clinton",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 3,
     image: Image3,
     title: "Sara Cill",
     description:
